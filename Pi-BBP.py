b46139b3cfb86c6715f6c0393804d49d50c2e954
daisaku-yokomatsu <daisaku.yokomatsu@gmail.com>
2021-01-30 22:23:57 -0900

    Add files via upload
    test

---
+++ Pi-BBP.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+from decimal import Decimal,getcontext
+
+getcontext().prec=1000
+
+print (sum(1/Decimal(16)**k * 
+          (Decimal(4)/(8*k+1) - 
+           Decimal(2)/(8*k+4) - 
+           Decimal(1)/(8*k+5) -
+           Decimal(1)/(8*k+6)) for k in range(1000))
+      )
+
+
+# In[ ]:
+
+
+
+
